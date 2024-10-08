fac49c84d9ccc37e18b66ba5ff4bad1e6d1ea665
Wichanan <wichananm65@nu.ac.th>
2024-10-08 17:49:55 +0700

    1.0.7

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import NoodleIndex from '@/components/Noodles/Index'
+import NoodleCreate from '@/components/Noodles/CreateNoodle'
+import NoodleEdit from '@/components/Noodles/EditNoodle'
+import NoodleShow from '@/components/Noodles/ShowNoodle'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/noodles',
+      name: 'noodles',
+      component: NoodleIndex
+    },
+    {
+      path: '/noodle/create',
+      name: 'noodle-create',
+      component: NoodleCreate
+    },
+    {
+      path: '/noodle/edit',
+      name: 'noodle-edit',
+      component: NoodleEdit
+    },
+    {
+      path: '/noodle',
+      name: 'noodle',
+      component: NoodleShow
     }
   ]
 })
